6341549cb569d98b99fe0e583c72fa0eb14885a1
Ronaldo Oliveira <ronaldo.oliveira@lut.com.br>
2017-02-07 13:50:05 -0200

    Acrescenta categorias: relacionamento com produtos e exibição da categoria de produto na lista

--- banco-produto.php
+++ banco-produto.php
@@ -2,7 +2,7 @@
 function listaProdutos($conexao)
 {
     $produtos = [];
-    $query = "select * from produtos";
+    $query = "select p.*, c.nome as categoria_nome from produtos p join categorias c on c.id = p.categoria_id";
     $resultado = mysqli_query($conexao, $query);
     while ($produto = mysqli_fetch_assoc($resultado)) {
         array_push($produtos, $produto);
@@ -11,9 +11,9 @@ function listaProdutos($conexao)
     return $produtos;
 }
 
-function insereProduto($conexao, $nome, $preco, $descricao)
+function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id)
 {
-    $query = "insert into produtos (nome, preco, descricao) values ('{$nome}', {$preco}, '{$descricao}')";
+    $query = "insert into produtos (nome, preco, descricao, categoria_id) values ('{$nome}', {$preco}, '{$descricao}', {$categoria_id})";
     return mysqli_query($conexao, $query);
 }
 
