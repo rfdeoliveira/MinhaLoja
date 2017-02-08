cfa5cec70e33bdbd52f9552ade0c8cf1838f6851
Ronaldo Oliveira <ronaldo.oliveira@lut.com.br>
2017-02-08 11:04:02 -0200

    Acrescenta formulário e funcionalidade para alterar produto no banco de dados

--- banco-produto.php
+++ banco-produto.php
@@ -12,11 +12,25 @@ function listaProdutos($conexao)
     return $produtos;
 }
 
+function buscaProduto($conexao, $id)
+{
+    $query = "select * from produtos where id = {$id}";
+    $resultado = mysqli_query($conexao, $query);
+    return mysqli_fetch_assoc($resultado);
+}
+
 function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado)
 {
     $query = "insert into produtos (nome, preco, descricao, categoria_id, usado) values ('{$nome}', {$preco}, '{$descricao}', {$categoria_id}, {$usado})";
     return mysqli_query($conexao, $query);
 }
+
+function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria_id, $usado)
+{
+    $query = "update produtos set nome = '{$nome}', preco = {$preco}, descricao = '{$descricao}', categoria_id = {$categoria_id} , usado = {$usado} where id = {$id}";
+    return mysqli_query($conexao, $query);
+}
+
 
 function removeProduto($conexao, $id)
 {
